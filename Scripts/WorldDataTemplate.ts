e03f08cf76f3d9689e71340bc3163b149d9f3af5
Titus <titusoosting@gmail.com>
2025-03-16 19:33:05 +0100

    fix world template

--- Scripts/WorldDataTemplate.ts
+++ Scripts/WorldDataTemplate.ts
@@ -14,6 +14,7 @@ export const worldTemplateString = `
 - **Time Formats:** {{time_format_names}}
 - **Time Format Equivalents:** {{time_format_equivalents}}
 - **Basic Time Unit:** {{time_basic_unit}}
+- **Time Range Min:** {{time_range_min}}
+- **Time Range Max:** {{time_range_max}}
 - **Current Time:** {{time_current}}
-- **Time Range:** From {{time_range_min}} to {{time_range_max}}
 `;
